15a88d3be7a382eea1f54fa3cd60b15214676cf3
Lucimerry <118849208+Lucimerry@users.noreply.github.com>
2024-10-04 11:24:43 -0300

    Update informacoesGlobais.js

--- grafico/informacoesGlobais.js
+++ grafico/informacoesGlobais.js
@@ -1,1 +1,23 @@
+const url = 'https://api.openaq.org/v2/measurements?city=São Paulo';
+
+async function visualizarInformacoesQualidadeAr() {
+    const res = await fetch(url);
+    const dados = await res.json();
+
+    const qualidadeAr = dados.results.map(resultado => ({
+        parameter: resultado.parameter,
+        value: resultado.value,
+        unit: resultado.unit,
+        date: resultado.date.local
+    }));
+
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML = `A qualidade do ar em São Paulo apresenta diferentes parâmetros: <br> ${qualidadeAr.map(q => `${q.parameter}: <span>${q.value} ${q.unit}</span> (${q.date})`).join('<br>')}`;
+
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
+}
+
+visualizarInformacoesQualidadeAr();
 
